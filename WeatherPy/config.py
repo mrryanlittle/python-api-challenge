22abd12672021a80c696e2920c4de00cae90d705
Ryan Little <mrryanlittle@gmail.com>
2021-02-09 17:00:12 -0700

    s_hemi_df cloudiness not working

--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "7ebd6c6580b89305f23a9df69d042cfc"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyA2VpqNjatbZpFBn6S18E3jkMK7-fx4kw0"
